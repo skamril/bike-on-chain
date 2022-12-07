2b35578a44d9ffde10b58c9220410159c6de3905
Pablohassan <rusmir-sadikovic_student2022@wilder.school>
2022-12-07 15:35:53 +0100

    resolution de conflicts entre les branches

--- hardhat/contracts/BikeOnChain.sol
+++ hardhat/contracts/BikeOnChain.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.17;
 import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
