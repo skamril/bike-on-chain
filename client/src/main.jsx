60f926baaf96bb56d6bc245d493ad77906c207e3
Pablohassan <rusmir-sadikovic_student2022@wilder.school>
2022-12-07 15:48:44 +0100

    --amend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import {ReactDOM, BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { WagmiConfig, createClient, chain } from "wagmi";
 import { ConnectKitProvider, getDefaultClient } from "connectkit";
 import App from "./App";
 import "./index.css";
 
-const alchemyId = import.meta.env.VITE_ALCHEMY_POL_MUM_ID;
+const alchemyId = import.meta.env.VITE_ALCHEMY_GOELRI_ID;
 const chains = [chain.mainnet,chain.goerli, chain.polygon, chain.polygonMumbai];
 const client = createClient(
   getDefaultClient({
