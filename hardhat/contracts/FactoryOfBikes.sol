f7887f961c6687a788730e9f161b25025e5315d2
Pablohassan <rusmir-sadikovic_student2022@wilder.school>
2022-12-11 11:50:38 +0100

    contrat factory

--- hardhat/contracts/FactoryOfBikes.sol
+++ hardhat/contracts/FactoryOfBikes.sol
@@ -2,45 +2,52 @@
 
 pragma solidity 0.8.17;
 import "./BikeOnChain.sol";
-import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
-contract FactoryOfBikes {
-  // Use the Counter contract to  track the number of contracts 
-  using Counters for Counters.Counter;
-// Store the addresses of the BikeOnChain contracts 
 
-event CreatedCollection(string _brandName, string _brandCollection);
- 
+/**
+ * @title BrandRegistry
+ * @dev This contract is used to create and manage instances of the BikeOnChain contract.
+ */
+contract FractoryOfBikes {
+  // Store the addresses of the BikeOnChain contracts
   BikeOnChain[] public bikeOnChainInstances;
-  
-  struct Brand {
-    string brandName;
-    string brandCollection;
-  }
 
-  Brand[] public brands;
-  event BikeContractCreated(address bikeOnChain);
-  Counters.Counter private contractIds;
+  // Mapping for brand collections to addresses public 
+  mapping(string => address) public brandCollections;
 
+  // Store brand names and collections in a mapping for BOC only 
+  mapping(string => string) private brands;
+
+  /**
+   * @dev Emits one event when a new BikeOnChain contract is created.
+   * @param bikeOnChain The address of created BikeOnChain contract.
+   */
+  event BikeContractCreated(address indexed bikeOnChain);
+
+  /**
+   * @dev Creates a new instance of the BikeOnChain contract.
+   * @param _brandName Name of the brand associated with the new contract.
+   * @param _brandCollection  ollection of the brand associated with the new contract.
+   */
   function createBikeOnChain(string memory _brandName, string memory _brandCollection) external {
-    require(keccak256(abi.encode(_brandName, _brandCollection)) != keccak256(abi.encode("")), 'Empty field');
+    require(keccak256(abi.encode(_brandName, _brandCollection)) != keccak256(abi.encode("")), 'Empty');
     // Create a new instance of the BikeOnChain contract
     BikeOnChain bikeOnChain = new BikeOnChain();
-    bikeOnChain.transferOwnership(msg.sender);
     bikeOnChainInstances.push(bikeOnChain);
-    contractIds.increment();
-    brands.push(Brand(_brandName, _brandCollection));
 
+    // Store the brand name and collection in the mapping
+    brands[_brandName] = _brandCollection;
+    // Add the brand collection to the mapping
+    brandCollections[_brandCollection] = address(bikeOnChain);
+    // Emit the event
     emit BikeContractCreated(address(bikeOnChain));
   }
 
+  /**
+   * @dev Returns the array of BikeOnChain contract addresses.
+   * @return The array of BikeOnChain contract addresses.
+   */
   function getBikeContracts() external view returns (BikeOnChain[] memory) {
     return bikeOnChainInstances;
   }
 }
-
-
-
-
-
-
