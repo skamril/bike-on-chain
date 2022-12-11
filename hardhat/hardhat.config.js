917dd17b88025e6ff9d4447b74aa28abbe340657
skamril <1954121+skamril@users.noreply.github.com>
2022-12-11 17:26:20 +0100

    feat(hardhat): create BocBikes contract

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    localhost: {
+      url: "HTTP://127.0.0.1:8545",
+      chainId: 31337
+    },
+    mumbai: {
+      url: process.env.ALCHEMY_RPC,
+      accounts: [process.env.ALCHEMY_API_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: true
+  }
 };
